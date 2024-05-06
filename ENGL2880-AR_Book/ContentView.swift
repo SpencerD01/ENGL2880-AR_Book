21c7fca54261428e31fb1ddab00da2e474a8f390
Spencer Dunn <smd326@cornell.edu>
2024-05-05 21:36:01 -0400

    Added plane detection stuff. Now highlights detected planes (both vertical and horizontal).

--- ENGL2880-AR_Book/ContentView.swift
+++ ENGL2880-AR_Book/ContentView.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import RealityKit
+import ARKit
 
 struct ContentView : View {
     var body: some View {
@@ -21,7 +22,7 @@ struct ARViewContainer: UIViewRepresentable {
         let arView = ARView(frame: .zero)
 
         // Create a cube model
-        let mesh = MeshResource.generateBox(size: 0.1, cornerRadius: 0.005)
+        /*let mesh = MeshResource.generateBox(size: 0.1, cornerRadius: 0.005)
         let material = SimpleMaterial(color: .gray, roughness: 0.15, isMetallic: true)
         let model = ModelEntity(mesh: mesh, materials: [material])
 
@@ -31,6 +32,21 @@ struct ARViewContainer: UIViewRepresentable {
 
         // Add the horizontal plane anchor to the scene
         arView.scene.anchors.append(anchor)
+         */
+        let session = arView.session
+        let config = ARWorldTrackingConfiguration()
+        config.planeDetection = [.vertical, .horizontal]
+        session.run(config)
+        
+        let coachingOverlay = ARCoachingOverlayView()
+        coachingOverlay.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        coachingOverlay.session = session
+        coachingOverlay.goal = .horizontalPlane
+        arView.addSubview(coachingOverlay)
+        
+        #if DEBUG
+        arView.debugOptions = [.showFeaturePoints, .showAnchorOrigins, .showAnchorGeometry]
+        #endif
 
         return arView
         
