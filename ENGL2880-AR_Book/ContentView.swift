dd1812db629ecfd3bc8e30f3b6581f806465d529
Spencer Dunn <smd326@cornell.edu>
2024-05-05 23:15:55 -0400

    Added tap to place block.

--- ENGL2880-AR_Book/ContentView.swift
+++ ENGL2880-AR_Book/ContentView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 import RealityKit
 import ARKit
+import FocusEntity
 
 struct ContentView : View {
     var body: some View {
@@ -21,21 +22,10 @@ struct ARViewContainer: UIViewRepresentable {
         
         let arView = ARView(frame: .zero)
 
-        // Create a cube model
-        /*let mesh = MeshResource.generateBox(size: 0.1, cornerRadius: 0.005)
-        let material = SimpleMaterial(color: .gray, roughness: 0.15, isMetallic: true)
-        let model = ModelEntity(mesh: mesh, materials: [material])
-
-        // Create horizontal plane anchor for the content
-        let anchor = AnchorEntity(.plane(.horizontal, classification: .any, minimumBounds: SIMD2<Float>(0.2, 0.2)))
-        anchor.children.append(model)
-
-        // Add the horizontal plane anchor to the scene
-        arView.scene.anchors.append(anchor)
-         */
         let session = arView.session
         let config = ARWorldTrackingConfiguration()
         config.planeDetection = [.vertical, .horizontal]
+        config.environmentTexturing = .automatic
         session.run(config)
         
         let coachingOverlay = ARCoachingOverlayView()
@@ -45,14 +35,62 @@ struct ARViewContainer: UIViewRepresentable {
         arView.addSubview(coachingOverlay)
         
         #if DEBUG
-        arView.debugOptions = [.showFeaturePoints, .showAnchorOrigins, .showAnchorGeometry]
+        arView.debugOptions = [.showAnchorGeometry]
         #endif
+        
+        // Handle ARSession events via delegate
+        context.coordinator.view = arView
+        session.delegate = context.coordinator
+        
+        // Handle taps
+        arView.addGestureRecognizer(
+            UITapGestureRecognizer(
+                target: context.coordinator,
+                action: #selector(Coordinator.handleTap)
+            )
+        )
 
         return arView
         
     }
     
     func updateUIView(_ uiView: ARView, context: Context) {}
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator()
+    }
+    
+    class Coordinator: NSObject, ARSessionDelegate {
+        weak var view: ARView?
+        var focusEntity: FocusEntity?
+
+        func session(_ session: ARSession, didAdd anchors: [ARAnchor]) {
+            guard let view = self.view else { return }
+            debugPrint("Anchors added to the scene: ", anchors)
+            self.focusEntity = FocusEntity(on: view, style: .classic(color: .yellow))
+        }
+        
+        @objc func handleTap() {
+            debugPrint("a")
+            guard let view = self.view, let focusEntity = self.focusEntity else { return }
+
+            // Create a new anchor to add content to
+            let anchor = AnchorEntity(/*AnchoringComponent.Target.plane(.any, classification: .any, minimumBounds: [0, 0])*/)
+            view.scene.anchors.append(anchor)
+
+            // Add a Box entity with a blue material
+            
+            let box = MeshResource.generateBox(size: 0.5, cornerRadius: 0.05)
+            let material = SimpleMaterial(color: .blue, isMetallic: true)
+            let diceEntity = ModelEntity(mesh: box, materials: [material])
+            diceEntity.position = focusEntity.position
+
+            anchor.addChild(diceEntity)
+            debugPrint("Done!")
+            
+            
+        }
+    }
     
 }
 
