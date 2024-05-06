6f3844b74dc3995bfa14f9f331a1e3c0f92da622
Spencer Dunn <smd326@cornell.edu>
2024-05-06 04:18:53 -0400

    idk what's going wrong

--- ENGL2880-AR_Book/ContentView.swift
+++ ENGL2880-AR_Book/ContentView.swift
@@ -70,12 +70,21 @@ struct ARViewContainer: UIViewRepresentable {
             //debugPrint("Anchors added to the scene: ", anchors)
         }
         
+        func textEntity (words: String, width: Float, height: Float) -> ModelEntity{
+            let text_frame = CGRect(x: Double(width / -2), y: Double(height / -1), width: Double(width), height: Double(height))
+            let textMesh = MeshResource.generateText(words, extrusionDepth: 10.15, font: UIFont.boldSystemFont(ofSize: 10), containerFrame: text_frame)
+            let entity = ModelEntity(mesh: textMesh)
+            let material = UnlitMaterial(color: .systemOrange)
+            entity.model?.materials = [material]
+            return entity
+        }
+        
         @objc func handleTap(_ sender: UITapGestureRecognizer) {
             guard let view = self.view else { return }
             debugPrint("New tap")
             
             let touchLocation = sender.location(in: view)
-            debugPrint(view.raycast(from: touchLocation, allowing: .existingPlaneGeometry, alignment: .any))
+            //debugPrint(view.raycast(from: touchLocation, allowing: .existingPlaneGeometry, alignment: .any))
             guard let raycastResult = view.raycast(from: touchLocation, allowing: .existingPlaneGeometry, alignment: .any).first else {
                 debugPrint("No surface detected, try getting closer.")
                 return
@@ -84,26 +93,61 @@ struct ARViewContainer: UIViewRepresentable {
             let int_anchor = raycastResult.anchor
             
             if let planeAnchor = int_anchor as? ARPlaneAnchor {
-                debugPrint(planeAnchor.planeExtent)
+                debugPrint(planeAnchor.planeExtent.width)
                 //guard let num_surfaces = self.surfaces?.count else { return <#default value#> }
-                
+                let pa_width = planeAnchor.planeExtent.width
+                let pa_height = planeAnchor.planeExtent.height
+                /*let translation = simd_float4(
+                    planeAnchor.center.y,
+                    planeAnchor.center.z,
+                    planeAnchor.center.x,
+                    0
+                )*/
+                let translation = simd_float4(
+                    planeAnchor.center.x,
+                    planeAnchor.center.y,
+                    planeAnchor.center.x,
+                    0
+                )
+                var transform = planeAnchor.transform
+                transform.columns.3 = transform.columns.3 - simd_make_float4(translation)
+                let z_angle = simd_float4x4(simd_quatf(angle: -1.5708, axis: simd_float3(1, 0, 0)))
+                transform = matrix_multiply(transform, z_angle)
                 
                 let anchor = AnchorEntity()
                 view.scene.anchors.append(anchor)
                 
-                let box = MeshResource.generateBox(width: Float(planeAnchor.planeExtent.width), height: Float(planeAnchor.planeExtent.height), depth: 0.01)
-                let material = SimpleMaterial(color: .white, isMetallic: false)
-                let text = MeshResource.generateText("Testing TESTING TESTRINGRTGDSKLFNSALKFJAKLF", extrusionDepth: 0.01, alignment: .center, lineBreakMode: .byWordWrapping)
-                let planeEntity = ModelEntity(mesh: box, materials: [material])
+                // Replace this with chosen poems
+                let inserted_text = "Test test 1 2 3 testing going on one two three"
+                
+                let text = textEntity(words: inserted_text, width: pa_width, height: pa_height)
+                //let offset = text.visualBounds(relativeTo: nil).extents.x / 2
+                //text.position = transform.worldTransform
+                debugPrint(text)
+                debugPrint(transform.columns.3)
+                text.transform = Transform(matrix: transform)
+                print(text.position)
+                print(anchor.position)
+                anchor.addChild(text)
+                
+                
+                /*let box = MeshResource.generateBox(width: Float(planeAnchor.planeExtent.width), height: Float(planeAnchor.planeExtent.height), depth: 0.01)
+                let material = UnlitMaterial(color: .systemOrange)
+                let text_frame = CGRect(x: Double(pa_width / -2), y: Double(pa_height / -1), width: Double(pa_width), height: Double(pa_height))
+                
+                
+                
+                let fontSize = (pa_width * pa_height) / Float(inserted_text.count)
+                let fontVar = UIFont.boldSystemFont(ofSize: 0.1)*/
+                //let text = MeshResource.generateText(inserted_text, extrusionDepth: 0.01, font: fontVar, containerFrame: text_frame, alignment: .left, lineBreakMode: .byWordWrapping)
+                
+                /*
+                let planeEntity = ModelEntity(mesh: text, materials: [material])
                 debugPrint(planeAnchor.transform)
-                let translation = simd_float3(
-                    planeAnchor.transform.columns.3.x,
-                    planeAnchor.transform.columns.3.y,
-                    planeAnchor.transform.columns.3.z
-                )
+                
                 debugPrint(planeAnchor.center)
                 var transform = planeAnchor.transform
-                transform.columns.3 = transform.columns.3 + simd_make_float4(planeAnchor.center * 2)
+                transform.columns.3 = transform.columns.3 - simd_make_float4(translation)
                 let z_angle = simd_float4x4(simd_quatf(angle: -1.5708, axis: simd_float3(1, 0, 0)))
                 transform = matrix_multiply(transform, z_angle)
                 
@@ -112,7 +156,10 @@ struct ARViewContainer: UIViewRepresentable {
                 planeEntity.transform = Transform(matrix: transform)
                 debugPrint(planeAnchor.transform)
                 debugPrint("New shape!!")
-                anchor.addChild(planeEntity)
+                
+                planeEntity.position = [-offset, -0.05, -0.1]
+                 */
+                
               }
             else {
                 debugPrint("Not a ARPlaneAnchor: ", type(of: int_anchor))
